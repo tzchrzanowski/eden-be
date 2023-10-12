4b83e5082c11eb736207ce38856ff28e0db5a8a8
tzchrzanowski <tzchrzanowski@gmail.com>
2023-10-12 10:06:59 +0800

    initial setup of eden-be app

---
+++ src/test/java/com/eden/edenbe/EdenBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eden.edenbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdenBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
