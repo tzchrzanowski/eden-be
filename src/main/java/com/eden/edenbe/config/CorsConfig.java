91ab252640adce1ac1c1e86c05caf7366601aac1
tzchrzanowski <tzchrzanowski@gmail.com>
2023-10-22 10:26:31 +0800

    CorsConfig cleanup

--- src/main/java/com/eden/edenbe/config/CorsConfig.java
+++ src/main/java/com/eden/edenbe/config/CorsConfig.java
@@ -1,19 +1,10 @@
 package com.eden.edenbe.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 @EnableWebMvc
@@ -23,7 +14,16 @@ public class CorsConfig implements WebMvcConfigurer {
         registry.addMapping("/api/**")
                 .allowedOrigins("http://localhost:3000")
                 .allowedMethods("PUT", "DELETE", "OPTIONS", "GET", "POST")
-                .allowedHeaders("Content-Type", "Credentials", "content-type", "credentials")
-                .allowCredentials(true).maxAge(3600);
+                .allowedHeaders("Content-Type", "Authorization")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
+
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeRequests()
+//                .requestMatchers("/api/public/**").permitAll();
+//        return http.build();
+//    }
 }
